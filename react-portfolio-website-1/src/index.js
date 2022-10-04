650316a521776ba1c4515cf76e7524168d3fbd90
Guzmance <00031119@uca.edu.sv>
2022-10-04 13:28:03 -0600

    Add: About Section

--- react-portfolio-website-1/src/index.js
+++ react-portfolio-website-1/src/index.js
@@ -1,5 +1,9 @@
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { createRoot } from 'react-dom/client';
 
-ReactDOM.render(<App/>, document.querySelector("#root"))
+//ReactDOM.render(<App/>, document.querySelector("#root"))
+const container = document.querySelector("#root");
+const root = createRoot(container);
+root.render(<App tab="home" />);
