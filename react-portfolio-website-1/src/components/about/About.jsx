152a3eb64a3a6b121b39299489640a696ca4936c
Guzmance <00031119@uca.edu.sv>
2022-12-01 02:11:03 -0600

    Add: Experience Section

--- react-portfolio-website-1/src/components/about/About.jsx
+++ react-portfolio-website-1/src/components/about/About.jsx
@@ -1,51 +1,54 @@
-import React from 'react'
-import './about.css'
-import ME from '../../assets/me-about.jpeg'
-import {FaAward} from 'react-icons/fa'
-import {FiUsers} from 'react-icons/fi'
-import {VscFolderLibrary} from 'react-icons/vsc'
+/* eslint-disable jsx-a11y/img-redundant-alt */
+import React from 'react';
+import './about.css';
+import ME from '../../assets/me-about.jpeg';
+import { FaAward } from 'react-icons/fa';
+import { FiUsers } from 'react-icons/fi';
+import { VscFolderLibrary } from 'react-icons/vsc';
 
 function About() {
   return (
-    <section id='about'>
+    <section id="about">
       <h5>Get To Know</h5>
       <h2>About Me</h2>
-      <div className='container about__container'>
-        <div className='about__me'>
+      <div className="container about__container">
+        <div className="about__me">
           <div className="about__me-image">
             <img src={ME} alt="About Image" />
           </div>
         </div>
-        <div className='about__content'>
+        <div className="about__content">
           <div className="about__cards">
-            <article className='about__card'>
-              <FaAward className='about__icon'/>
+            <article className="about__card">
+              <FaAward className="about__icon" />
               <h5>Experience</h5>
               <small>1+ Years Working</small>
             </article>
-            <article className='about__card'>
-              <FiUsers className='about__icon'/>
+            <article className="about__card">
+              <FiUsers className="about__icon" />
               <h5>Clients</h5>
               <small>0+ Worldwide</small>
             </article>
-            <article className='about__card'>
-              <VscFolderLibrary className='about__icon'/>
+            <article className="about__card">
+              <VscFolderLibrary className="about__icon" />
               <h5>Projects</h5>
               <small>1+ Completed Projects</small>
             </article>
           </div>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-            Iure iste tenetur omnis quos possimus qui id corporis praesentium 
-            illo amet dolorum deserunt voluptas suscipit, excepturi dolore aliquid 
-            totam voluptatibus similique.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure iste
+            tenetur omnis quos possimus qui id corporis praesentium illo amet
+            dolorum deserunt voluptas suscipit, excepturi dolore aliquid totam
+            voluptatibus similique.
           </p>
 
-          <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+          <a href="#contact" className="btn btn-primary">
+            Let's Talk
+          </a>
         </div>
       </div>
     </section>
-  )
+  );
 }
 
-export default About
+export default About;
