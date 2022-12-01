9c89a44bb4008619e579d9a7bb2cda38b20f1404
Guzmance <00031119@uca.edu.sv>
2022-12-01 02:43:34 -0600

    :zzz: Redy to continue

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -1,18 +1,30 @@
-import React from 'react'
-import {BsLinkedin} from 'react-icons/bs'
-import {FaGithub} from 'react-icons/fa'
-import {BsFacebook} from 'react-icons/bs'
-import {BsInstagram} from 'react-icons/bs'
+import React from 'react';
+import { BsLinkedin } from 'react-icons/bs';
+import { FaGithub } from 'react-icons/fa';
+import { BsFacebook } from 'react-icons/bs';
+import { BsInstagram } from 'react-icons/bs';
 
 const HeaderSocials = () => {
   return (
-    <div className='header__socials'>
-        <a href="htttps://linkedin.com" target="_blank"><BsLinkedin/></a>
-        <a href="htttps://github.com" target="_blank"><FaGithub/></a>
-        <a href="htttps://instagram.com" target="_blank"><BsInstagram/></a>
-        <a href="htttps://facebook.com" target="_blank"><BsFacebook/></a>
+    <div className="header__socials">
+      <a href="htttps://linkedin.com" rel="noopener noreferrer" target="_blank">
+        <BsLinkedin />
+      </a>
+      <a href="htttps://github.com" rel="noopener noreferrer" target="_blank">
+        <FaGithub />
+      </a>
+      <a
+        href="htttps://instagram.com"
+        rel="noopener noreferrer"
+        target="_blank"
+      >
+        <BsInstagram />
+      </a>
+      <a href="htttps://facebook.com" rel="noopener noreferrer" target="_blank">
+        <BsFacebook />
+      </a>
     </div>
-  )
-}
+  );
+};
 
-export default HeaderSocials
+export default HeaderSocials;
