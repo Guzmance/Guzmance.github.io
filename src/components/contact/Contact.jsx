0172090a173e2ed430cd01f8a65d86c817c5607d
Carlos Enrique Guzman Espinoza <00031119@uca.edu.sv>
2023-07-18 17:09:27 -0600

    Add: Contact Section

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,10 +1,99 @@
-import React from 'react'
-import './contact.css'
-
+import React, { useRef } from 'react';
+import './contact.css';
+import { MdOutlineEmail } from 'react-icons/md';
+import { RiMessengerLine } from 'react-icons/ri';
+import { BsWhatsapp } from 'react-icons/bs';
+import emailjs from 'emailjs-com';
 function Contact() {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        'service_n7gi72x',
+        'template_wo3spw3',
+        form.current,
+        'vZw1-axDTwYCUIJrI'
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+
+    e.target.reset();
+  };
   return (
-    <section id='contact'>Contact</section>
-  )
+    <section id="contact">
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact_container">
+        <div className="contact_options">
+          <article className="contact_option">
+            <MdOutlineEmail className="contact_option-icon" />
+            <h4>Email</h4>
+            <h5>carlose.guzmane62@hotmail.com</h5>
+            <a
+              href="mailto:carlose.guzmane62@hotmail.com"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Send a Message
+            </a>
+          </article>
+          <article className="contact_option">
+            <RiMessengerLine className="contact_option-icon" />
+            <h4>Messenger</h4>
+            <h5>Carlos Guzman</h5>
+            <a
+              href="https://m.me/100002365985513"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Send a Message
+            </a>
+          </article>
+          <article className="contact_option">
+            <BsWhatsapp className="contact_option-icon" />
+            <h4>Whatsapp</h4>
+            <h5>+(503) 7469-1631</h5>
+            <a
+              href="https://api.whatsapp.com/send?phone=+50374691631"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Send a Message
+            </a>
+          </article>
+        </div>
+        {/*End Of Contact Options*/}
+        <form ref={form} onSubmit={sendEmail}>
+          <input
+            type="text"
+            name="name"
+            placeholder="Your Full Name"
+            required
+          />
+          <input type="email" name="email" placeholder="Your Email" required />
+          <textarea
+            name="message"
+            placeholder="Your Message"
+            rows="10"
+            required
+          ></textarea>
+          <button type="submit" className="btn btn-primary">
+            Send Message
+          </button>
+        </form>
+      </div>
+    </section>
+  );
 }
 
-export default Contact
+export default Contact;
