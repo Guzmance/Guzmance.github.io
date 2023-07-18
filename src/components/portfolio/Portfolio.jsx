ca2263eb2ae721c72004318561443ca57c5bd109
Carlos Enrique Guzman Espinoza <00031119@uca.edu.sv>
2023-07-17 22:48:51 -0600

    Add: Portfolio Items Section

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,10 +1,84 @@
-import React from 'react'
-import './portfolio.css'
+import React from 'react';
+import './portfolio.css';
+import IMG1 from '../../assets/portfolio1.jpg';
+import IMG2 from '../../assets/portfolio2.jpg';
+import IMG3 from '../../assets/portfolio3.jpg';
+import IMG4 from '../../assets/portfolio4.jpg';
+import IMG5 from '../../assets/portfolio5.png';
+import IMG6 from '../../assets/portfolio6.jpg';
+
+const data = [
+  {
+    id: 1,
+    image: IMG1,
+    title: 'Crypto Currency Dashboard n Financial Visualization',
+    github: 'https://github.com',
+    demo: 'https://cdn.dribbble.com/userupload/8642428/file/original-ec9481b0c996eed61fe6d7dcaad18971.png?resize=752x',
+  },
+  {
+    id: 2,
+    image: IMG2,
+    title: 'Crypto Currency Dashboard n Financial Visualization',
+    github: 'https://github.com',
+    demo: 'https://cdn.dribbble.com/userupload/8642428/file/original-ec9481b0c996eed61fe6d7dcaad18971.png?resize=752x',
+  },
+  {
+    id: 3,
+    image: IMG3,
+    title: 'Crypto Currency Dashboard n Financial Visualization',
+    github: 'https://github.com',
+    demo: 'https://cdn.dribbble.com/userupload/8642428/file/original-ec9481b0c996eed61fe6d7dcaad18971.png?resize=752x',
+  },
+  {
+    id: 4,
+    image: IMG4,
+    title: 'Crypto Currency Dashboard n Financial Visualization',
+    github: 'https://github.com',
+    demo: 'https://cdn.dribbble.com/userupload/8642428/file/original-ec9481b0c996eed61fe6d7dcaad18971.png?resize=752x',
+  },
+  {
+    id: 5,
+    image: IMG5,
+    title: 'Crypto Currency Dashboard n Financial Visualization',
+    github: 'https://github.com',
+    demo: 'https://cdn.dribbble.com/userupload/8642428/file/original-ec9481b0c996eed61fe6d7dcaad18971.png?resize=752x',
+  },
+  {
+    id: 6,
+    image: IMG6,
+    title: 'Crypto Currency Dashboard n Financial Visualization',
+    github: 'https://github.com',
+    demo: 'https://cdn.dribbble.com/userupload/8642428/file/original-ec9481b0c996eed61fe6d7dcaad18971.png?resize=752x',
+  },
+];
 
 const Portfolio = () => {
   return (
-    <section id='portfolio'>Portfolio</section>
-  )
-}
+    <section id="portfolio">
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+      <div className="container portfolio_container">
+        {data.map(({ id, title, image, github, demo }) => {
+          return (
+            <article key={id} className="portfolio_item">
+              <div className="portfolio_item-image">
+                <img src={image} alt={title} />
+              </div>
+              <h3>{title}</h3>
+              <div className="portfolio_item-cta">
+                <a href={github} className="btn">
+                  Github
+                </a>
+                <a href={demo} className="btn btn-primary">
+                  Live Demo
+                </a>
+              </div>
+            </article>
+          );
+        })}
+      </div>
+    </section>
+  );
+};
 
-export default Portfolio
+export default Portfolio;
