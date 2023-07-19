8675177f84f15ae87273a75d6c763b1db0cfeb64
Carlos Enrique Guzman Espinoza <00031119@uca.edu.sv>
2023-07-18 22:00:34 -0600

    Add: Navigation Fixed

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -6,22 +6,24 @@ import HeaderSocials from './HeaderSocials';
 
 const Header = () => {
   return (
-    <header>
-      <div className="container header__container">
-        <h5>Hello I'm</h5>
-        <h1>Carlos Guzmán</h1>
-        <h5 className="text-light">Software Engineer</h5>
-        <CTA />
-        <HeaderSocials />
-        <div className="me">
-          <img src={ME} alt="me" />
-        </div>
+    <section id="home">
+      <header>
+        <div className="container header__container">
+          <h5>Hello I'm</h5>
+          <h1>Carlos Guzmán</h1>
+          <h5 className="text-light">Software Engineer</h5>
+          <CTA />
+          <HeaderSocials />
+          <div className="me">
+            <img src={ME} alt="me" />
+          </div>
 
-        <a href="#contact" className="scroll__down">
-          Scroll Down
-        </a>
-      </div>
-    </header>
+          <a href="#contact" className="scroll__down">
+            Scroll Down
+          </a>
+        </div>
+      </header>
+    </section>
   );
 };
 
